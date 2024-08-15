4d6ae7cfc338f6e891c50f8442ec489bd262b752
brunosalvogoncalves <128438721+brunosalvogoncalves@users.noreply.github.com>
2024-08-15 20:19:23 -0300

    15/08

---
+++ main.js
@@ -0,0 +1,25 @@
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixaprincipal = document.querySelector(".caixa-alternativas");
+const caixaprincipal = document.querySelector(".caixa-resultado");
+const caixaprincipal = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+    {
+       enunciado: "Pergunta 1",
+       alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+       [
+    }
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+             "alternativa 1"
+             "alternativa 2"
+        ]
+    }
+];
+
+let atual = 0;
+let perguntaAtual;
